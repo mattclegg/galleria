aaa77238cdba7d3c5ad791fb68eab66fa6df4a75
Victor Berchet <victor.berchet@yahoo.com>
2010-07-26 12:46:30 +0200

    Take outer height into account to compute thumbs height

--- src/galleria.js
+++ src/galleria.js
@@ -783,7 +783,7 @@ var G = window.Galleria = Base.extend({
             if (thumb.ready) {
                 w += thumb.outerWidth || this.width(thumb.elem, true);
                 hooks[i+1] = w;
-                h = Math.max(h, thumb.image.height)
+                h = Math.max(h, thumb.outerHeight || this.height(thumb.elem, true))
             }
         });
         this.toggleClass(this.get('thumbnails-container'), 'galleria-carousel', w > this.stageWidth);
